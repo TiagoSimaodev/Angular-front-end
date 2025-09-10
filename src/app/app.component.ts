7506865f9b09919dd99e6b5369ed0a969f055bd8
tiago simao <tiagosimaorodri123@gmail.com>
2025-09-09 23:43:18 -0300

    feat: adiciona navbar com bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { LoginServiceService } from './service/login-service.service';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  
   title = 'projeto-angular-rest';
+ 
+  constructor (private router: Router) {
+
+  }
+ 
+  ngOnInit(): void {
+    if(localStorage.getItem('token') == null) {
+      this.router.navigate(['login']);
+    }
+
+  }
+
+  public sair() {
+    localStorage.clear();
+    this.router.navigate(['login']);
+  }
 
   
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import {RouterModule, Routes} from '@angular/router'
 import { ModuleWithProviders } from '@angular/compiler/src/core';
 import { NgModule } from '@angular/core';
 import { LoginComponent } from './login/login.component';
+import { HttpInterceptorModule } from './service/header-interceptor.service';
 
 export const appRouters: Routes = [
   {path : 'home', component : HomeComponent},
@@ -27,7 +28,8 @@ export const routes : ModuleWithProviders = RouterModule.forRoot(appRouters);
    BrowserModule,
     FormsModule,
     HttpClientModule,
-    routes
+    routes,
+    HttpInterceptorModule
     
   ],
   providers: [],
